136f9c9ecbbde0710cd4851073b7cbcb0ce1d153
Hemant <20211489@sbsstc.ac.in>
2024-02-28 21:45:33 +0530

    created api routes

--- api/index.js
+++ api/index.js
@@ -1,19 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 // connecting the mongodb database with the backend service
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("connected");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 // initialise the the backend express server
 const app = express();
 
-//allocated the local port to the server  
+//allocated the local port to the server
 app.listen(3001, () => {
   console.log("server is running on port 3001!");
 });
+
+app.use("/api/user", userRouter);
