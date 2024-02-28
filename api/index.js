b7956189910215c7df74cb1f5ff96b39f29f081f
Hemant <20211489@sbsstc.ac.in>
2024-02-28 21:17:30 +0530

    created user model

--- api/index.js
+++ api/index.js
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
+// connecting the mongodb database with the backend service
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected");
 }).catch((err) => {
     console.log(err);
 })
 
+// initialise the the backend express server
 const app = express();
 
+//allocated the local port to the server  
 app.listen(3001, () => {
   console.log("server is running on port 3001!");
 });
