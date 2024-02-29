1dd74bd642dcffbc66877cdb485e6a8e4d724c4c
Hemant <20211489@sbsstc.ac.in>
2024-02-29 15:46:02 +0530

    implemented error handler

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -27,4 +27,14 @@ app.listen(3001, () => {
 });
 
 app.use("/api/user", userRouter);
-app.use('/api/auth' , authRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal sever error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
