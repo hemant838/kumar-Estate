19266629f8b5e60c9014092687e54e2de405d4a7
Hemant <20211489@sbsstc.ac.in>
2024-03-07 01:44:28 +0530

    added redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Link , useNavigate} from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { signInStart } from "../redux/user/userSlice";
-import { signInSuccess } from "../redux/user/userSlice";
-import { signInFailure } from "../redux/user/userSlice";
+import { signInStart,signInSuccess,signInFailure } from "../redux/user/userSlice";
 
 function Signin() {
   const [formData, setFormData] = useState({});
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,10 +21,10 @@ const userSlice = createSlice({
     signInFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
-    },
-  },
+    }
+  }
 });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
-export const {actions, reducer:userReducer} = userSlice
+export const {actions, reducer:userReducer} = userSlice;
 export default userSlice.reducer;
